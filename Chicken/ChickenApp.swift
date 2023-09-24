5a0656ab251991b202c92c59d6b630f840003127
XiaYue <admin@xiayue.tw>
2023-09-24 02:51:42 -0700

    Initial Commit

---
+++ Chicken/ChickenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenApp.swift
+//  Chicken
+//
+//  Created by 朱信宇 on 2023/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ChickenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chicken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chicken
+//
+//  Created by 朱信宇 on 2023/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
